2e2840fdb3689ae252817597f45d7e58174fbcf8
enzo.mazotti <enzo.mazotti@students.ffhs.ch>
2024-04-08 14:57:54 +0200

    chng from gradle to maven

---
+++ src/test/java/ch/ffhs/savvy/SavvyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ffhs.savvy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavvyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
