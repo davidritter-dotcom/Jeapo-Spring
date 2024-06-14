741c57626e88d9bbdf97459ff011f7ad71f8380f
David Ritter <david.ritter@students.ffhs.ch>
2024-06-14 02:02:52 +0200

    add vaadin dependencie

--- src/main/java/ch/ffhs/savvy/savvyspring/SavvySpringApplication.java
+++ src/main/java/ch/ffhs/savvy/savvyspring/SavvySpringApplication.java
@@ -1,9 +1,11 @@
 package ch.ffhs.savvy.savvyspring;
 
+import com.vaadin.flow.spring.annotation.EnableVaadin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableVaadin(value = "ch.ffhs.savvy.savvyspring.view")
 public class SavvySpringApplication {
 
 	public static void main(String[] args) {
