f19861a24c0fe25e3bcd919db30e695c819513ea
David Ritter <david.ritter@students.ffhs.ch>
2024-06-15 14:48:36 +0200

    now it's the initial commit ;)

---
+++ src/main/java/ch/ffhs/savvy_spring/SavvySpringApplication.java
@@ -0,0 +1,13 @@
+package ch.ffhs.savvy_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavvySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavvySpringApplication.class, args);
+	}
+
+}
